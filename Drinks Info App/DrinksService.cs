3878fda04f9428ca1b583dbf0c0aa473a37ec719
LuisMerc4do <163725779+LuisMerc4do@users.noreply.github.com>
2024-06-14 19:26:28 +1000

    Finished Project and Comments

--- Drinks Info App/DrinksService.cs
+++ Drinks Info App/DrinksService.cs
@@ -1,20 +1,22 @@
 ﻿using Drinks_Info_App.Models;
 using Newtonsoft.Json;
 using RestSharp;
+using System.Reflection;
 using System.Web;
-
 
 namespace Drinks_Info_App
 {
     public class DrinksService
     {
-        public List<Category>GetCategories()
+        // Method to get the list of categories
+        public List<Category> GetCategories()
         {
             var client = new RestClient("https://www.thecocktaildb.com/api/json/v1/1/");
             var request = new RestRequest("list.php?c=list");
             var response = client.ExecuteAsync(request);
             List<Category> categories = new();
 
+            // If the response is successful
             if (response.Result.StatusCode == System.Net.HttpStatusCode.OK)
             {
                 string rawResponse = response.Result.Content;
@@ -22,58 +24,73 @@ namespace Drinks_Info_App
 
                 categories = serialize.CategoriesList;
 
+                // Display the categories in a table
                 TableVisualisationEngine.ShowTable(categories, "Categories Menu");
                 return categories;
             }
             return categories;
         }
-    
-    internal List<Drink> GetDrinksByCategory(string category)
-    {
-        var client = new RestClient("http://www.thecocktaildb.com/api/json/v1/1/");
-        var request = new RestRequest($"filter.php?c={HttpUtility.UrlEncode(category)}");
 
-        var response = client.ExecuteAsync(request);
-
-        List<Drink> drinks = new();
-
-        if (response.Result.StatusCode == System.Net.HttpStatusCode.OK)
-        {
-            string rawResponse = response.Result.Content;
-
-            var serialize = JsonConvert.DeserializeObject<Drinks>(rawResponse);
-
-            drinks = serialize.DrinksList;
-
-            TableVisualisationEngine.ShowTable(drinks, "Drinks Menu");
-
-            return drinks;
-
-        }
-
-        return drinks;
-        }
-    internal void GetDrink(string drink)
+        // Method to get the list of drinks by category
+        internal List<Drink> GetDrinksByCategory(string category)
         {
             var client = new RestClient("http://www.thecocktaildb.com/api/json/v1/1/");
             var request = new RestRequest($"filter.php?c={HttpUtility.UrlEncode(category)}");
             var response = client.ExecuteAsync(request);
-            List<DrinkDetail> drinkDetail = new();
+            List<Drink> drinks = new();
 
+            // If the response is successful
             if (response.Result.StatusCode == System.Net.HttpStatusCode.OK)
             {
                 string rawResponse = response.Result.Content;
-
                 var serialize = JsonConvert.DeserializeObject<Drinks>(rawResponse);
 
                 drinks = serialize.DrinksList;
 
+                // Display the drinks in a table
                 TableVisualisationEngine.ShowTable(drinks, "Drinks Menu");
-
                 return drinks;
+            }
+            return drinks;
+        }
 
+        // Method to get details of a specific drink
+        internal void GetDrink(string drink)
+        {
+            var client = new RestClient("http://www.thecocktaildb.com/api/json/v1/1/");
+            var request = new RestRequest($"lookup.php?i={drink}");
+            var response = client.ExecuteAsync(request);
+
+            // If the response is successful
+            if (response.Result.StatusCode == System.Net.HttpStatusCode.OK)
+            {
+                string rawResponse = response.Result.Content;
+                var serialize = JsonConvert.DeserializeObject<DrinkDetailObject>(rawResponse);
+                List<DrinkDetail> returnedList = serialize.DrinkDetailList;
+                DrinkDetail drinkDetail = returnedList[0];
+                List<object> prepList = new();
+                string formattedName = "";
+
+                // Prepare the drink details for display
+                foreach (PropertyInfo prop in drinkDetail.GetType().GetProperties())
+                {
+                    if (prop.Name.Contains("str"))
+                    {
+                        formattedName = prop.Name.Substring(3);
+                    }
+                    if (!string.IsNullOrEmpty(prop.GetValue(drinkDetail)?.ToString()))
+                    {
+                        prepList.Add(new
+                        {
+                            Key = formattedName,
+                            Value = prop.GetValue(drinkDetail)
+                        });
+                    }
+                }
+
+                // Display the drink details in a table
+                TableVisualisationEngine.ShowTable(prepList, drinkDetail.strDrink);
             }
         }
     }
-
 }
--- Drinks Info App/UserInput.cs
+++ Drinks Info App/UserInput.cs
@@ -4,34 +4,45 @@
     {
         DrinksService drinksService = new();
 
+        // Method to get the categories input from the user
         public void GetCategoriesInput()
         {
+            // Retrieve and display categories
             drinksService.GetCategories();
             Console.WriteLine("Choose Category");
 
+            // Variable to store the user input and a flag for input validity
             string? readResult = null;
             bool validResult = false;
             string categorySelection = "";
+
+            // Loop until valid input is received
             while (!validResult)
             {
                 readResult = Console.ReadLine();
                 if (readResult != null)
                 {
                     categorySelection = readResult.Trim();
-                    validResult = true; 
+                    validResult = true;
                 }
             }
+
+            // Get the drinks for the selected category
             GetDrinksInput(categorySelection);
         }
 
+        // Method to get the drinks input from the user for a selected category
         private void GetDrinksInput(string category)
         {
+            // Retrieve and display drinks for the selected category
             var drinks = drinksService.GetDrinksByCategory(category);
 
             Console.WriteLine("Choose a drink or go back to category menu by typing 0:");
             string? readResult = null;
             bool validResult = false;
             string drinkSelection = "";
+
+            // Loop until valid input is received
             while (!validResult)
             {
                 readResult = Console.ReadLine();
@@ -41,21 +52,24 @@
                     validResult = true;
                 }
             }
+
+            // If the user wants to go back to the categories menu
             if (drinkSelection == "0") GetCategoriesInput();
 
+            // If the selected drink does not exist
             if (!drinks.Any(x => x.idDrink == drinkSelection))
             {
                 Console.WriteLine("Drink doesn't exist.");
                 GetDrinksInput(category);
             }
 
-
+            // Retrieve and display details of the selected drink
             drinksService.GetDrink(drinkSelection);
 
+            // Prompt the user to go back to categories menu
             Console.WriteLine("Press any key to go back to categories menu");
             Console.ReadKey();
             if (!Console.KeyAvailable) GetCategoriesInput();
-
         }
     }
 }
